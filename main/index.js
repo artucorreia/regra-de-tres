c12001662facce167101622c15b78778d44b0f92
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2022-10-23 18:40:52 -0300

    first commit

---
+++ main/index.js
@@ -0,0 +1,12 @@
+var botao = window.document.getElementById('botao');
+botao.addEventListener('click', clicar);
+function clicar(){
+    var valorUm = window.document.getElementById('primeiro');
+    var numUm = Number(valorUm.value);
+    var valorDois = window.document.getElementById('segundo');
+    var numDois = Number(valorDois.value);
+    var valorTres = window.document.getElementById('terceiro');
+    var numTres = Number(valorTres.value);
+    var valorQuatro = window.document.getElementById('quarto');
+    var numQuatro = Number(valorQuatro.value); 
+}
