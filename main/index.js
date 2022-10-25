06714a2e5cfc3088bbc6cb9ff47e446d3e10b1d7
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2022-10-25 12:47:18 -0300

    Update index.js

--- main/index.js
+++ main/index.js
@@ -1,5 +1,7 @@
 var botao = window.document.getElementById('botao');
 botao.addEventListener('click', setarValores);
+
+// Setar Valores
 let valorUm = window.document.getElementById('primeiro');
 let valorDois = window.document.getElementById('segundo');
 let valorTres = window.document.getElementById('terceiro');
@@ -11,13 +13,51 @@ function setarValores() {
     numero[2] = Number(valorTres.value);
     numero[3] = Number(valorQuatro.value); 
     calculo();
+    console.log(numero[0])
+    console.log(numero[3])
 }
 
-function calculo(numUm, numQuatro) {
-    for (let i = 0; i <= 3; i++) {
-        if (numero[i]) {
 
+// Cálculo
+let termoUm = false;
+let posX = 0;
+let primeiraParte = 0;
+let segundaParte = 0;
+let resp = 0;
+function calculo() {
+    for (let i = 0; i <= 3; i++) {
+        if (numero[i] == 0) {
+            posX = i
         }
-        
     }
+    if ((posX == 0) || (posX == 3)) {
+        termoUm = true;
+        if (posX == 0) {
+            primeiraParte = numero[3];
+        } else {
+            primeiraParte = numero[0];
+        }
+    } else {
+        primeiraParte = numero[0] * numero[3];
+    }
+    if ((posX == 1) || (posX == 2)) {
+        termoUm = false;
+        if (posX == 1) {
+            segundaParte = numero[2]; 
+        } else {
+            segundaParte = numero[1];
+        }
+    } else {
+        segundaParte = numero[1] * numero[2];
+    }
+    if (termoUm == true) {
+        alert(primeiraParte + 'x = ' + segundaParte);
+        resp = segundaParte / primeiraParte;
+        alert(resp);
+    } else {
+        alert(primeiraParte + ' = ' + segundaParte + 'x');
+        resp = primeiraParte / segundaParte;
+        alert(resp);
+    }
+    termoUm = false;
 }
