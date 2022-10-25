37b9de4f9fb563bce3fbf16dccd0c9a94550405f
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2022-10-25 11:20:30 -0300

    Update index.js

--- main/index.js
+++ main/index.js
@@ -1,12 +1,23 @@
 var botao = window.document.getElementById('botao');
-botao.addEventListener('click', clicar);
-function clicar(){
-    var valorUm = window.document.getElementById('primeiro');
-    var numUm = Number(valorUm.value);
-    var valorDois = window.document.getElementById('segundo');
-    var numDois = Number(valorDois.value);
-    var valorTres = window.document.getElementById('terceiro');
-    var numTres = Number(valorTres.value);
-    var valorQuatro = window.document.getElementById('quarto');
-    var numQuatro = Number(valorQuatro.value); 
+botao.addEventListener('click', setarValores);
+let valorUm = window.document.getElementById('primeiro');
+let valorDois = window.document.getElementById('segundo');
+let valorTres = window.document.getElementById('terceiro');
+let valorQuatro = window.document.getElementById('quarto');
+let numero = [0, 0, 0, 0]
+function setarValores() {
+    numero[0] = Number(valorUm.value);
+    numero[1] = Number(valorDois.value);
+    numero[2] = Number(valorTres.value);
+    numero[3] = Number(valorQuatro.value); 
+    calculo();
+}
+
+function calculo(numUm, numQuatro) {
+    for (let i = 0; i <= 3; i++) {
+        if (numero[i]) {
+
+        }
+        
+    }
 }
